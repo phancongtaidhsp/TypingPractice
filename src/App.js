573c5960209ae610be8de68bbf5855a96573ae99
phancongtaidhsp <phancongtaicntt2@gmail.com>
2020-04-05 00:21:50 +0700

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Word from './components/Word/Word';
 import Letter from './components/Letter/Letter';
 
-let text = 'Stronger unpacked felicity to of mistaken. Fanny at wrong table ye in. Be on easily cannot innate in lasted months on. Differed and and felicity steepest mrs age outweigh. Opinions learning likewise daughter now age outweigh. Raptures stanhill my greatest mistaken or exercise he on although. Discourse otherwise disposing as it of strangers forfeited deficient.'
-const arrLetter = text.split(''); 
-const body = document.getElementById('bodyApp');
+class App extends Component {
 
-function App() {
+  constructor(props){
+    super(props);
+    let text = 'Stronger unpacked felicity to of mistaken. Fanny at wrong table ye in. Be on easily cannot innate in lasted months on. Differed and and felicity steepest mrs age outweigh. Opinions learning likewise daughter now age outweigh. Raptures stanhill my greatest mistaken or exercise he on although. Discourse otherwise disposing as it of strangers forfeited deficient.'
+    this.arrLetter = text.split('');
+    this.arrWord = text.split(' ').map(word => [...word.split(''),' ']);
+    this.arrWord[this.arrWord.length-1].pop();
+    this.state = {
+      currentLetter: 0
+    };
+  }
 
-  const [currentLetter, setCurrentLetter] = useState(0);
-
-  body.onkeydown = (event) => {
-    if(event.key === arrLetter[currentLetter]){
-      setCurrentLetter(currentLetter+1)
+  componentDidMount(){
+    const body = document.getElementById('bodyApp');
+    body.onkeydown = (event) => {
+      if(event.key === this.arrLetter[this.state.currentLetter]){
+        this.setState({
+          currentLetter: this.state.currentLetter + 1
+        })
+      }
     }
   }
 
-  return (
-    <div className="App">
-      {arrLetter.map((letter,index) => {
-        const isActived = index === currentLetter ? true : false;
-        return <Letter currentLetter={currentLetter} index={index} key={index}>{letter}</Letter>
-      })}
-    </div>
-    
-  );
+  render(){
+    const { currentLetter } = this.state;
+    let counter = -1;
+    return (
+      <div className="App">
+        {this.arrWord.map(word =>
+          <Word key={counter}>
+            {word.map(letter => {
+              counter++;
+              return <Letter currentLetter={currentLetter} index={counter} key={counter}>{letter}</Letter>
+            })}
+          </Word>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Word.css'
 
 function Word(props) {
   return (
-    <div>
+    <div className='word'>
       {props.children}
     </div>
   )
