0ba3d92964ecbbb38ad38bec44b89ef744c81a12
Linh Nguyen <linhqng207@gmail.com>
2020-06-19 09:13:39 +0700

    add homepage ui and get api random text

--- src/App.js
+++ src/App.js
@@ -1,45 +1,66 @@
-import React, { Component } from 'react';
-import './App.css';
-import Word from './components/Word/Word';
-import Letter from './components/Letter/Letter';
+import React, { Component } from "react";
+import "./App.css";
+import Word from "./components/Word/Word";
+import Letter from "./components/Letter/Letter";
+import HomePage from "./pages/homepage.component";
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    let text = 'Stronger unpacked felicity to of mistaken. Fanny at wrong table ye in. Be on easily cannot innate in lasted months on. Differed and and felicity steepest mrs age outweigh. Opinions learning likewise daughter now age outweigh. Raptures stanhill my greatest mistaken or exercise he on although. Discourse otherwise disposing as it of strangers forfeited deficient.'
-    this.arrLetter = text.split('');
-    this.arrWord = text.split(' ').map(word => [...word.split(''),' ']);
-    this.arrWord[this.arrWord.length-1].pop();
+  constructor() {
+    super();
     this.state = {
-      currentLetter: 0
+      currentLetter: 0,
+      text: [],
     };
   }
 
-  componentDidMount(){
-    const body = document.getElementById('bodyApp');
-    body.onkeydown = (event) => {
-      if(event.key === this.arrLetter[this.state.currentLetter]){
-        this.setState({
-          currentLetter: this.state.currentLetter + 1
-        })
-      }
-    }
+  componentDidMount() {
+    fetch(
+      "https://baconipsum.com/api/?type=all-meat&paras=10&start-with-lorem=1"
+    )
+      .then((response) => response.json())
+      .then((result) => this.setState({ text: result }));
+
+    // this.arrLetter = this.text.split("");
+    // this.arrWord = this.text.split(" ").map((word) => [...word.split(""), " "]);
+    // this.arrWord[this.arrWord.length - 1].pop();
+
+    // const body = document.getElementById("bodyApp");
+    // body.onkeydown = (event) => {
+    //   if (event.key === this.arrLetter[this.state.currentLetter]) {
+    //     this.setState({
+    //       currentLetter: this.state.currentLetter + 1,
+    //     });
+    //   }
+    // };
   }
 
-  render(){
-    const { currentLetter } = this.state;
-    let counter = -1;
+  render() {
+    // const { currentLetter } = this.state;
+    // let counter = -1;
+    // return (
+    //   <div className="App">
+    //     {this.arrWord.map((word) => (
+    //       <Word key={counter}>
+    //         {word.map((letter) => {
+    //           counter++;
+    //           return (
+    //             <Letter
+    //               currentLetter={currentLetter}
+    //               index={counter}
+    //               key={counter}
+    //             >
+    //               {letter}
+    //             </Letter>
+    //           );
+    //         })}
+    //       </Word>
+    //     ))}
+    //   </div>
+    // );
+
     return (
-      <div className="App">
-        {this.arrWord.map(word =>
-          <Word key={counter}>
-            {word.map(letter => {
-              counter++;
-              return <Letter currentLetter={currentLetter} index={counter} key={counter}>{letter}</Letter>
-            })}
-          </Word>
-        )}
+      <div>
+        <HomePage></HomePage>
       </div>
     );
   }
---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./homepage.styles.css";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="header">
+      <div className="logo">
+        <h1>Typing</h1>
+        <sub>.Practice</sub>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
