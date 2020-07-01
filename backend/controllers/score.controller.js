fcd3bf54feec0680074b6c4c873ba3bc58127a7d
phancongtaidhsp <phancongtaicntt2@gmail.com>
2020-07-01 17:23:57 +0700

    add getRankByLessonId

--- backend/controllers/score.controller.js
+++ backend/controllers/score.controller.js
@@ -11,3 +11,16 @@ module.exports.saveScore = async (req, res) => {
   })
   res.status(200).send('Save successful')
 }
+
+module.exports.getRankByLessonId = async (req, res) => {
+  var lesson_id = req.body.params.id;
+  Score.find({lesson_id: lesson_id}).then(scores => {
+    scores.sort((a,b) => {
+      return parseInt(b.score) - parseInt(a.score)
+    })
+    if(scores.length > 4) scores = scores.slice(0,4);
+    res.status(200).send(scores);
+  },error => {
+    res.status(500).send(error);
+  })
+}
